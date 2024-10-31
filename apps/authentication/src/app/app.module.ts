e5fc90e913c9ec33708868f94041db7767c876e2
Matthias Biebl <matthias.biebl@datev.de>
2024-10-31 12:19:16 +0100

    add mongoose module to jest app authentication

--- apps/authentication/src/app/app.module.ts
+++ apps/authentication/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+      MongooseModule.forRoot('mongodb://localhost:27017/auth-workshop'),
+      ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: '.env',
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
